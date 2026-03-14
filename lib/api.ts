e05b72fce62201bf1ad8fe67b00d85d7d1af82ca
gusdev07 <giancarlopalvess@outlook.com>
2026-03-14 02:16:49 -0300

    last version

--- lib/api.ts
+++ lib/api.ts
@@ -1,4 +1,4 @@
-export const BASE_URL = 'http://localhost:3001';
+export const BASE_URL = 'https://clip-generator-geraew-api-provider.ernvcw.easypanel.host';
 
 export interface AuthUser {
   id: string;
